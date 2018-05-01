3eeb0da4f1215d5f7b66ab9f14b7f754960d1feb
watanabeyu <fight.ippatu5648@gmail.com>
2018-05-01 12:34:55 +0900

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* client */
 import Client from './src/client';
-import TWLoginButton from './src/button';
+import TWLoginButton from './src/Button';
 import {
   decodeHTMLEntities,
   getRelativeTime,
